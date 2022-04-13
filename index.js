6fd6d6f2607a0c810f9b615df8d1fa2537f4dffd
Niraj Kumar Roy <nirajroy149@gmail.com>
2022-04-13 19:45:26 +0530

    added keyboard events and changed styling

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 
 $("button").click(play);
 
-function play(){
 
+// for click 
+function play() {
     playaudio(this.innerHTML);
 }
 
-function playaudio(key){
+//for keyboard press
+document.addEventListener("keydown", function (event) {
+    playaudio(event.key);  
+})
+
+function animatebutton(key) {
+    $("." + key).addClass("pressed");
+    setTimeout(function(){
+        $("." + key).removeClass("pressed");
+    },100);
+}
+function playaudio(key) {
 
     let audio = new Audio();
-    let src ="";
+    let src = "";
 
-    switch(key){
+    switch (key) {
         case "w":
-            src = "tom-1.mp3";
+            src = "tom-1.mp3";animatebutton("w");
             break;
         case "a":
-            src = "tom-2.mp3";
+            src = "tom-2.mp3";animatebutton("a");
             break;
         case "s":
-            src = "tom-3.mp3";
+            src = "tom-3.mp3";animatebutton("s");
             break;
         case "d":
-            src = "tom-4.mp3";
+            src = "tom-4.mp3";animatebutton("d");
             break;
         case "j":
-            src = "crash.mp3";
+            src = "crash.mp3";animatebutton("j");
             break;
         case "k":
-            src = "kick-bass.mp3";
+            src = "kick-bass.mp3";animatebutton("k");
             break;
         case "l":
-            src = "snare.mp3";
+            src = "snare.mp3";animatebutton("l");
             break;
         default:
-            console.log("not found");
+            console.log("Not found");
             break;
     }
     audio.src = "sounds/" + src;
